8755ab285a4712db681619dae3ae374597bd2be9
Saara Satokangas <saarasat@gmail.com>
2021-02-22 17:42:45 +0200

    Exercise 11.6

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
   "env": {
     "browser": true,
+    "node": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
   },
   "extends": [
     "eslint:recommended",
@@ -44,6 +45,6 @@ module.exports = {
       "error", { "before": true, "after": true }
     ],
     "no-console": "error",
-    "react/prop-types": 0
+    "react/prop-types": 0,
   }
 }
