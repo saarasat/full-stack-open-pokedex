a7c2955fe26a23acaedc13f965f16e18b80fb640
Saara Satokangas <saarasat@gmail.com>
2021-02-22 19:04:58 +0200

    Exercise 11.9

--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -5,7 +5,13 @@ const PokemonList = ({ pokemonList }) => {
   return (
     <div className="list-container">
       {pokemonList.map(({ id, name }) => (
-        <Link key={id} to={`/pokemon/${name}`} className="list-item" style={{ backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`})` }}>
+        <Link
+          key={id}
+          to={`/pokemon/${name}`}
+          id={`link-${name}`}
+          className="list-item"
+          style={{ backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`})` }}
+        >
           <div
             className="list-item-name"
           >
